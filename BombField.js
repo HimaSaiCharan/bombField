7642a70091650d23f34a78a87c60e2fdac0ad189
Hima Sai Charan <127401464+HimaSaiCharan@users.noreply.github.com>
2024-11-22 18:51:39 +0530

    Update BombField.js

--- BombField.js
+++ BombField.js
@@ -16,7 +16,11 @@ function getRowFooting() {
   return '┣━━━━╋━━━━╋━━━━╋━━━━╋━━━━╋━━━━╋━━━━╋━━━━╋━━━━╋━━━━┫';
 }
 
-function getCharsInCell(p1Pos,cellNumber, bombBoxNumber) {
+function getCharsInCell(p1Pos, cellNumber, bombBoxNumber, winBoxNumber) {
+  if (cellNumber === winBoxNumber) {
+    return ' ' + '🎉' + ' ┃';
+  }
+
   if (cellNumber === bombBoxNumber) {
     return ' ' + '💥' + ' ┃';
   }
@@ -27,21 +31,21 @@ function getCharsInCell(p1Pos,cellNumber, bombBoxNumber) {
   return ' ⬜ ┃';
 }
 
-function createRow(p1Pos,rowStartNumber, bombBoxNumber) {
+function createRow(p1Pos, rowStartNumber, bombBoxNumber, winBox) {
   let rowString = '';
 
   for (let boxNumber = rowStartNumber; boxNumber > rowStartNumber - 10; boxNumber--) {
-    rowString += getCharsInCell(p1Pos,boxNumber, bombBoxNumber);
+    rowString += getCharsInCell(p1Pos, boxNumber, bombBoxNumber, winBox);
   }
 
   return '┃' + rowString;
 }
 
-function createGrids(p1Pos,bombBox) {
+function createGrids(p1Pos, bombBox, winBox) {
   let grid = getHeading() + '\n';
 
   for (let noOfRows = 10; noOfRows > 0; noOfRows--) {
-    grid += createRow(p1Pos,noOfRows * 10, bombBox) + '\n';
+    grid += createRow(p1Pos, noOfRows * 10, bombBox, winBox) + '\n';
 
     if (noOfRows !== 1) {
       grid += getRowFooting() + '\n';
@@ -98,14 +102,14 @@ function getEndPosition() {
 }
 
 function getStartPosition() {
-  const position = +prompt("Enter your starting position(1-10): ", "0");
+  const position = +prompt("Enter your starting position(1-10): ", "01");
 
   if (position < 10 && position > 0) {
     return position;
   }
 
-  console.log("Starting position can only be between 1 and 10");
-  console.log("Enter the valid position!!!");
+  console.log("Starting position can only be between 1 and 10..🙄");
+  console.log("Enter a valid position..!");
 
   return getStartPosition();
 }
@@ -139,17 +143,23 @@ function getKey() {
     console.log("Please enter an Valid key..");
     return getKey();
   }
-  
+
   return keyPressed;
 }
 
-function getNextPosition(currentPosition) {
+function getNextPosition(currentPosition, destination) {
   const key = getKey();
 
   switch (key) {
     case 'a':
+      if (currentPosition + 1 > 100) {
+        return currentPosition;
+      }
       return currentPosition + 1;
     case 'w':
+      if (currentPosition + 10 !== destination && currentPosition + 10 > 100) {
+        return currentPosition;
+      }
       return currentPosition + 10;
     case 'd':
       return currentPosition - 1;
@@ -160,11 +170,8 @@ function getNextPosition(currentPosition) {
 
 function startGame() {
   const bombPositions = generateBombs();
-  // console.log(bombPositions);
-
   const endPosition = getEndPosition();
-  // console.log(endPosition);
-  
+
   let startPosition = 0;
   console.log(createGrids());
 
@@ -175,19 +182,21 @@ function startGame() {
     console.clear();
 
     if (isBombEncountered(startPosition, bombPositions)) {
-      console.log(createGrids(startPosition, startPosition));
-      prompt("Ohh..You encountered a Bomb💥..Press enter");
+      console.log(createGrids('', startPosition));
+      console.log("Ohh..You encountered a Bomb💥💥..");
       startPosition = 0;
 
       continue;
     }
-    
+
     console.log(createGrids(startPosition));
     console.log("a : ⬅️   w : ⬆️   d : ➡️  s : ⬇️");
 
-    startPosition = getNextPosition(startPosition);
+    startPosition = getNextPosition(startPosition, endPosition);
   }
-  console.log("Congrats..🎉..You found the destination🤩🥳") 
+  console.clear();
+  console.log(createGrids('', '', startPosition - 10));
+  console.log("Congrats..🎉..You found the destination🤩🥳");
 }
 
 startGame();
