06dc4ad0a3ee25610a0b30b14b2bbf40c4039490
Hima Sai Charan <127401464+HimaSaiCharan@users.noreply.github.com>
2024-11-22 17:33:50 +0530

    Update BombField.js

--- BombField.js
+++ BombField.js
@@ -1,4 +1,5 @@
-const p1Name = '😁';
+
+const p1Name = '🤩';
 const p2Name = '😎';
 const p3Name = '😴';
 const p4Name = '😳';
@@ -15,46 +16,32 @@ function getRowFooting() {
   return '┣━━━━╋━━━━╋━━━━╋━━━━╋━━━━╋━━━━╋━━━━╋━━━━╋━━━━╋━━━━┫';
 }
 
-function getCharsInRow(p1Pos, p2Pos, p3Pos, p4Pos, boxNumber, bombBoxNumber) {
-  if (boxNumber === bombBoxNumber) {
-    return ' ' + '💥' + ' ┃' ;
+function getCharsInCell(p1Pos,cellNumber, bombBoxNumber) {
+  if (cellNumber === bombBoxNumber) {
+    return ' ' + '💥' + ' ┃';
   }
 
-  if (p1Pos === boxNumber) {
-    return ' ' + p1Name + ' ┃' ;
+  if (p1Pos === cellNumber) {
+    return ' ' + p1Name + ' ┃';
   }
-
-  if (p2Pos === boxNumber) {
-    return ' ' + p2Name + ' ┃' ;
-  }
-
-  if (p3Pos === boxNumber) {
-    return ' ' + p3Name + ' ┃' ;
-  }
-
-  if (p4Pos === boxNumber) {
-    return ' ' + p4Name + ' ┃' ;
-  }
-
   return ' ⬜ ┃';
 }
 
-
-function createRow(p1Pos, p2Pos, p3Pos, p4Pos, rowStartNumber, bombBoxNumber) {
+function createRow(p1Pos,rowStartNumber, bombBoxNumber) {
   let rowString = '';
 
   for (let boxNumber = rowStartNumber; boxNumber > rowStartNumber - 10; boxNumber--) {
-    rowString += getCharsInRow(p1Pos, p2Pos, p3Pos, p4Pos, boxNumber, bombBoxNumber);
+    rowString += getCharsInCell(p1Pos,boxNumber, bombBoxNumber);
   }
 
   return '┃' + rowString;
 }
 
-function createGrids(p1Pos, p2Pos, p3Pos, p4Pos, bombBox) {
+function createGrids(p1Pos,bombBox) {
   let grid = getHeading() + '\n';
 
   for (let noOfRows = 10; noOfRows > 0; noOfRows--) {
-    grid += createRow(p1Pos, p2Pos, p3Pos, p4Pos, noOfRows * 10, bombBox) + '\n';
+    grid += createRow(p1Pos,noOfRows * 10, bombBox) + '\n';
 
     if (noOfRows !== 1) {
       grid += getRowFooting() + '\n';
@@ -64,8 +51,143 @@ function createGrids(p1Pos, p2Pos, p3Pos, p4Pos, bombBox) {
   return grid + getFooting();
 }
 
-function updatePlayersPosition (p1Pos, p2Pos, ) {
-  return createGrids(12, 32, 54, 76, 12);
+function getRandomNoInRange(to, from) { // 90, 100
+  return from + Math.ceil(Math.random() * (to - from));
 }
 
-console.log(12, 32, 54, 76);
+function getBombPosition() {
+  return Math.ceil(Math.random() * 100);
+}
+
+function isCorrectBombPosition(bombPosition) {
+
+  let correctPosition = bombPosition - 11 !== '🧨';
+  correctPosition = correctPosition && bombPosition - 10 !== '🧨';
+  correctPosition = correctPosition && bombPosition - 9 !== '🧨';
+
+  correctPosition = correctPosition && bombPosition - 1 !== '🧨';
+  correctPosition = correctPosition && bombPosition + 1 !== '🧨';
+
+  correctPosition = correctPosition && bombPosition + 9 !== '🧨';
+  correctPosition = correctPosition && bombPosition + 10 !== '🧨';
+  correctPosition = correctPosition && bombPosition + 11 !== '🧨';
+
+  return correctPosition;
+}
+
+function generateBombs() {
+  let totalNoOfBobms = 20;
+  let bombPosition = '';
+
+  while (totalNoOfBobms > 0) {
+    bombPosition += getBombPosition() + ' ';
+    if (isCorrectBombPosition(bombPosition)) {
+      totalNoOfBobms -= 1;
+    }
+  }
+
+  return bombPosition;
+}
+
+function didPlayerWin(position, endPosition) {
+  return position === endPosition;
+}
+
+function getEndPosition() {
+  return getRandomNoInRange(100, 110);
+}
+
+function getStartPosition() {
+  const position = +prompt("Enter your starting position(1-10): ", "0");
+
+  if (position < 10 && position > 0) {
+    return position;
+  }
+
+  console.log("Starting position can only be between 1 and 10");
+  console.log("Enter the valid position!!!");
+
+  return getStartPosition();
+}
+
+function isBombEncountered(position, bombPositions) {
+  let currentBombPos = '';
+
+  for (let index = 0; index < bombPositions.length; index++) {
+    currentBombPos += bombPositions[index];
+
+    if (bombPositions[index] === ' ') {
+      if (+currentBombPos === position) {
+        return true;
+      }
+      currentBombPos = '';
+    }
+
+  }
+
+  return false;
+}
+
+function iskeyPressedValid(key) {
+  return key === 'a' || key === 's' || key === 'd' || key === 'w';
+}
+
+function getKey() {
+  const keyPressed = prompt("Enter a key to move..");
+
+  if (!iskeyPressedValid(keyPressed)) {
+    console.log("Please enter an Valid key..");
+    return getKey();
+  }
+  
+  return keyPressed;
+}
+
+function getNextPosition(currentPosition) {
+  const key = getKey();
+
+  switch (key) {
+    case 'a':
+      return currentPosition + 1;
+    case 'w':
+      return currentPosition + 10;
+    case 'd':
+      return currentPosition - 1;
+    case 's':
+      return currentPosition - 10;
+  }
+}
+
+function startGame() {
+  const bombPositions = generateBombs();
+  // console.log(bombPositions);
+
+  const endPosition = getEndPosition();
+  // console.log(endPosition);
+  
+  let startPosition = 0;
+  console.log(createGrids());
+
+  while (!didPlayerWin(startPosition, endPosition)) {
+    if (startPosition < 1) {
+      startPosition = getStartPosition();
+    }
+    console.clear();
+
+    if (isBombEncountered(startPosition, bombPositions)) {
+      console.log(createGrids(startPosition, startPosition));
+      prompt("Ohh..You encountered a Bomb💥..Press enter");
+      startPosition = 0;
+
+      continue;
+    }
+    
+    console.log(createGrids(startPosition));
+    console.log("a : ⬅️   w : ⬆️   d : ➡️  s : ⬇️");
+
+    startPosition = getNextPosition(startPosition);
+  }
+  console.log("Congrats..🎉..You found the destination🤩🥳") 
+}
+
+startGame();
